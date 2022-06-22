322e30c2c6c7bdbcb324277cb8fc31dd440886d0
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-06-22 18:17:48 +0530

    BarChart Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import { React } from 'react';
+import React from 'react';
+import BarChart from './BarChart';
 import './App.scss';
 
+const barData = {
+	values: [
+		{ year: '2016', number: 1.2 },
+		{ year: '2017', number: 1.9 },
+		{ year: '2018', number: 3.1 },
+		{ year: '2019', number: 4.8 },
+		{ year: '2020', number: 6.9 },
+		{ year: '2021', number: 11.3 },
+	],
+};
 const App = () =>
 	<div className="App" role="App">
-		Ready to start.
+		<BarChart data={ barData }/>,
 	</div>;
 
 export default App;
---
+++ src/BarChart.js
@@ -0,0 +1,16 @@
+import { createClassFromLiteSpec } from 'react-vega-lite';
+
+export default createClassFromLiteSpec('BarChart', {
+	description: 'A simple bar chart with embedded data.',
+	width: 400,
+	height: 400,
+	mark: { type: 'bar', width: { band: 0.7 }},
+	encoding: {
+		x: { timeUnit: 'year',
+			field: 'year',
+			type: 'temporal' },
+		y: { field: 'number',
+			type: 'quantitative',
+			title: 'Number of Vehicles in Millions' },
+	},
+});
