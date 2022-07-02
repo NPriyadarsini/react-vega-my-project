e92e9972754f8bcd13e95d7f976b03a66b8f0d40
Priyadarsini_n <priyadarsini.nkp@gmail.com>
2022-07-02 19:16:15 +0530

    Refactoring

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -1,17 +1,25 @@
-import { createClassFromLiteSpec } from 'react-vega-lite';
+import React from 'react';
+import { VegaLite } from 'react-vega';
 
-export default createClassFromLiteSpec('DonutChart', {
-	description: 'A simple Donut Chart',
-	width: 400,
-	height: 400,
-	mark: { type: 'arc', innerRadius: 50, outerRadius: 90 },
-	title: 'No of Vehicles WorldWide(Millions)',
-	encoding: {
-		color: { timeUnit: 'year',
-			field: 'year',
-			type: 'nominal' },
-		theta: { field: 'number',
-			type: 'quantitative',
-			stack: true },
-	},
-});
+const DonutChart = ({ data }) => {
+	const spec = {
+		description: 'A simple Donut Chart',
+		width: 400,
+		height: 400,
+		mark: { type: 'arc', innerRadius: 90 },
+		title: 'No of Vehicles WorldWide(Millions)',
+		encoding: {
+			color: { timeUnit: 'year',
+				field: 'year',
+				type: 'nominal' },
+			theta: { field: 'number',
+				type: 'quantitative',
+				stack: true },
+		},
+		data: { name: 'values' },
+	};
+
+	return <VegaLite { ...{ spec, data } }/>;
+};
+
+export default DonutChart;
--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,25 +1,21 @@
-import { createClassFromLiteSpec } from 'react-vega-lite';
+import React from 'react';
+import { VegaLite } from 'react-vega';
 
-export default createClassFromLiteSpec('PieChart', {
-	description: 'A simple Pie chart',
-	width: 400,
-	height: 400,
-	mark: 'arc',
-	title: 'No of Vehicles WorldWide(Millions)',
-	encoding: {
-		color: { timeUnit: 'year',
-			field: 'year',
-			type: 'nominal' },
-		theta: { field: 'number',
-			type: 'quantitative',
-			stack: true },
-	},
-	layer: [{
-		mark: { type: 'arc', outerRadius: 90 },
-	}, {
-		mark: { type: 'text', radius: 100 },
+const PieChart = ({ data }) => {
+	const spec = {
+		description: 'A simple Pie chart',
+		width: 400,
+		height: 400,
+		mark: { type: 'arc', tooltip: true },
+		title: 'No of Vehicles WorldWide(Millions)',
 		encoding: {
-			text: { field: 'number', type: 'nominal' },
+			color: { timeUnit: 'year', field: 'year', type: 'nominal' },
+			theta: { field: 'number', type: 'quantitative', stack: true },
 		},
-	}],
-});
+		data: { name: 'values' },
+	};
+
+	return <VegaLite { ...{ spec, data } }/>;
+};
+
+export default PieChart;
