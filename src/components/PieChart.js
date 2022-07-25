708284eba0474bcf9dd55996fcb086df0968be4c
njj <njj@laufire.in>
2022-07-25 12:10:07 +0530

    Highlight while select.

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import { VegaLite } from 'react-vega';
 
@@ -6,11 +7,24 @@ const PieChart = ({ config: { chartProps: { width, autosize }}, data }) => {
 		description: 'A simple Pie chart',
 		width: width,
 		autosize: autosize,
+		params: [{ name: 'highlight', select: 'point' }],
 		mark: { type: 'arc', tooltip: true },
 		title: 'No of Vehicles WorldWide(Millions)',
 		encoding: {
 			color: { timeUnit: 'year', field: 'year', type: 'nominal' },
 			theta: { field: 'number', type: 'quantitative', stack: true },
+			stroke: {
+				condition: {
+					param: 'highlight',
+					empty: false,
+					value: 'black',
+				},
+				value: null,
+			},
+			opacity: {
+				condition: { param: 'highlight', value: 1 },
+				value: 0.5,
+			},
 		},
 		data: { name: 'values' },
 	};
